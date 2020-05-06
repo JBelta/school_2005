957d22ef3cfcd49148c0dc7eee06987997cee7ca
James Belta <james.belta@gmail.com>
2020-05-06 13:38:09 -0600

    Removed pry, line 61

--- test/school_test.rb
+++ test/school_test.rb
@@ -58,7 +58,7 @@ class SchoolTest < Minitest::Test
     school.add_student_name('Aurora')
     school.add_student_name('tim')
     school.add_student_name('megan')
-require 'pry'; binding.pry
+
     assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
   end
 
